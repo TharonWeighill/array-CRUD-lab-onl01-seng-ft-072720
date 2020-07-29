f7176b27689c2cf5683034f85576cf4ccaf2a07b
Tharon Weighill <tharonweighill@gmail.com>
2020-07-29 23:38:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,6 +27,6 @@ def remove_element_from_start_of_array (a)
 wow = a.shift
 end
 
-def retrieve_element_from_index (a, 
+def retrieve_element_from_index (a, 2)
   a = ["wow", "I", "am", "really", "learning", "arrays!"]
   
